8386b5f0e628ad841b338cf7ee40e39191cc1ec6
ZoePlayoust <playoustz@gmail.com>
2022-05-05 19:21:31 +0200

    Update image.js

--- image.js
+++ image.js
@@ -45,8 +45,7 @@
 // var img2 = new MyObjectConstructor(2, `<img src="img/2.jpg" id="imgtwo">`);
 // var img3 = new MyObjectConstructor(3, `<img src="img/3.jpg" id="imgthree">`);
 // var img4 = new MyObjectConstructor(
-//   4,
-//   `<div class="image-container" id="imgs"><img src="img/4.jpg id="imgfour" /></div>`
+
 // );
 // let imageCollection = [img1, img2, img3, img4];
 
@@ -57,47 +56,73 @@ let btnPrev = document.querySelector("#left");
 let btnNext = document.querySelector("#right");
 let image = document.querySelector("#imgs");
 
-let image1 = `<img src="img/1.jpg" id="imgone">`;
-let image2 = `<img src="img/2.jpg" id="imgtwo">`;
-let image3 = `<img src="img/3.jpg" id="imgthree">`;
-let image4 = `<img src="img/4.jpg" id="imgfour">`;
+function MyObjectConstructor(source, id) {
+  // this is a pointer that points to the reference of that object
+  this.source = source;
+  this.id = id;
+  this.sayAllInOne = function () {
+    return `<img src="${this.source}" id="${this.id}">`;
+  };
+}
+
+let image1 = new MyObjectConstructor("img/1.jpg", "imgone");
+let image2 = new MyObjectConstructor("img/2.jpg", "imgtwo");
+let image3 = new MyObjectConstructor("img/3.jpg", "imgthree");
+let image4 = new MyObjectConstructor("img/4.jpg", "imgfour");
 
 btnNext.addEventListener("click", clickNext);
 btnPrev.addEventListener("click", clickPrevious);
 // btnPrev.addEventListener("click", clickPrev);
 
-function clickNext(event) {
-  if (image.innerHTML.trim() === image1) {
-    image.innerHTML = image2;
+let gallery = [image1, image2, image3, image4];
+let index = 1;
+
+function clickNext() {
+  if (index === 4) {
+    index = 1;
   } else {
-    if (image.innerHTML.trim() === image2) {
-      image.innerHTML = image3;
-    } else {
-      if (image.innerHTML.trim() === image3) {
-        image.innerHTML = image4;
-      } else {
-        if (image.innerHTML.trim() === image4) {
-          image.innerHTML = image1;
-        }
-      }
-    }
+    index = index + 1;
   }
+  image.innerHTML = gallery[index - 1].sayAllInOne();
 }
 
-function clickPrevious(event) {
-  if (image.innerHTML.trim() === image1) {
-    image.innerHTML = image4;
+function clickPrevious() {
+  if (index === 1) {
+    index = 4;
   } else {
-    if (image.innerHTML.trim() === image2) {
-      image.innerHTML = image1;
-    } else {
-      if (image.innerHTML.trim() === image3) {
-        image.innerHTML = image2;
-      } else {
-        if (image.innerHTML.trim() === image4) {
-          image.innerHTML = image3;
-        }
-      }
-    }
+    index = index - 1;
   }
+  image.innerHTML = gallery[index - 1].sayAllInOne();
 }
+
+// let image1 = `<img src="img/1.jpg" id="imgone">`;
+// let image2 = `<img src="img/2.jpg" id="imgtwo">`;
+// let image3 = `<img src="img/3.jpg" id="imgthree">`;
+// let image4 = `<img src="img/4.jpg" id="imgfour">`;
+
+// function clickNext(event) {
+//   if (image.innerHTML.trim() === image1.sayAllInOne()) {
+//     image.innerHTML = image2.sayAllInOne();
+//   } else {
+//     if (image.innerHTML.trim() === image2) {
+//       image.innerHTML = image3;
+//     } else {
+//       if (image.innerHTML.trim() === image3) {
+//         image.innerHTML = image4;
+//       } else {
+//         if (image.innerHTML.trim() === image4) {
+//           image.innerHTML = image1;
+//         }
+//       }
+//     }
+//   }
+// }
+
+// function clickNext() {
+//   if (image.innerHTML.trim() === gallery[0].sayAllInOne()) {
+//     image.innerHTML = gallery[1].sayAllInOne();
+//   } else {
+//     image.innerHTML.trim() === gallery[1].sayAllInOne();
+//     image.innerHTML = gallery[2].sayAllInOne();
+//   }
+// }
